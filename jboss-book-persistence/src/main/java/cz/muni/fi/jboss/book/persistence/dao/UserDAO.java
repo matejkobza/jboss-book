618828debb7dd11fdabef0fb25c85b246c85eb7e
Matej Kobza <mates@ravers.sk>
2013-04-30 21:58:05 +0200

    project structure

---
+++ jboss-book-persistence/src/main/java/cz/muni/fi/jboss/book/persistence/dao/UserDAO.java
@@ -0,0 +1,18 @@
+package cz.muni.fi.jboss.book.persistence.dao;
+
+import cz.muni.fi.library.entity.User;
+import java.util.List;
+
+/**
+ *
+ * @author Eduard Tomek
+ */
+public interface UserDAO {
+   
+    User createUser(User user);
+    User updateUser(User user);
+    void deleteUser(User user);
+    User findUserById(Long id);
+    User findUserByUsername(String username);
+    List<User> findAllUsers(); 
+}
---
+++ jboss-book-persistence/src/main/java/cz/muni/fi/jboss/book/persistence/enum/ReservationState.java
@@ -0,0 +1,19 @@
+/**
+ * ReservationState.java
+ *
+ * @author Eduard Tomek
+ */
+package cz.muni.fi.library.enums;
+
+/**
+ * Enum ReservationState
+ * represents states in which reservation can be
+ *
+ * @author Eduard Tomek
+ *
+ */
+public enum ReservationState {
+	
+	NEW_RESERVATION, READY_TO_LEND, LENDED, RETURNED
+
+}
