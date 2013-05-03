8c1d3c1809efbeb2a4ab031b57e2a2df62319f37
Svata Novak <324881@mail.muni.cz>
2013-05-03 16:01:25 +0200

    Added the rest of the BookDao methods to BookManager

--- ejb/ejbModule/cz/muni/fi/library/ejb/managers/BookManager.java
+++ ejb/ejbModule/cz/muni/fi/library/ejb/managers/BookManager.java
@@ -1,9 +1,19 @@
 package cz.muni.fi.library.ejb.managers;
 
+import java.util.List;
+
 import cz.muni.fi.jboss.book.persistence.entity.Book;
 
 public interface BookManager {
 
-	public abstract Book addBook(Book book);
+	public Book addBook(Book book);
+	public Book updateBook(Book book);
+	public void deleteBook(Book book);
+	
+	public Book findBookById(Long id);
+	public List<Book> findBookByISBN(Long ISBN);
+	public List<Book> findBookByTitle(String title);
+	public List<Book> findBookByAuthor(String author);
+	public List<Book> findAllBooks();
 
 }
--- ejb/ejbModule/cz/muni/fi/library/ejb/managers/BookManagerImpl.java
+++ ejb/ejbModule/cz/muni/fi/library/ejb/managers/BookManagerImpl.java
@@ -1,5 +1,7 @@
 package cz.muni.fi.library.ejb.managers;
 
+import java.util.List;
+
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.enterprise.context.Dependent;
@@ -10,7 +12,7 @@ import cz.muni.fi.jboss.book.persistence.dao.BookDAO;
 import cz.muni.fi.jboss.book.persistence.entity.Book;
 
 @Dependent
-@Named
+@Named("bookManager")
 @Stateless
 @Remote
 public class BookManagerImpl implements BookManager {
@@ -18,12 +20,44 @@ public class BookManagerImpl implements BookManager {
 	@Inject
 	private BookDAO bookDao;
 
-	/* (non-Javadoc)
-	 * @see cz.muni.fi.library.ejb.managers.BookManager#addBook(cz.muni.fi.jboss.book.persistence.entity.Book)
-	 */
 	@Override
 	public Book addBook(Book book) {
 		return bookDao.createBook(book);
+	}
+
+	@Override
+	public Book updateBook(Book book) {
+		return bookDao.updateBook(book);
+	}
+
+	@Override
+	public void deleteBook(Book book) {
+		bookDao.deleteBook(book);
+	}
+
+	@Override
+	public Book findBookById(Long id) {
+		return bookDao.findBookById(id);
+	}
+
+	@Override
+	public List<Book> findBookByISBN(Long ISBN) {
+		return bookDao.findBookByISBN(ISBN);
+	}
+
+	@Override
+	public List<Book> findBookByTitle(String title) {
+		return bookDao.findBookByTitle(title);
+	}
+
+	@Override
+	public List<Book> findBookByAuthor(String author) {
+		return bookDao.findBookByAuthor(author);
+	}
+
+	@Override
+	public List<Book> findAllBooks() {
+		return bookDao.findAllBooks();
 	}
 
 }
