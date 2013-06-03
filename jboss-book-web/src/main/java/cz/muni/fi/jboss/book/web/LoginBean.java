3609dc29dd2a7e408d35e1576bd929b685d03616
Svata Novak <324881@mail.muni.cz>
2013-06-03 19:38:36 +0200

    Tried to implement login in...

--- jboss-book-web/src/main/java/cz/muni/fi/jboss/book/web/LoginBean.java
+++ jboss-book-web/src/main/java/cz/muni/fi/jboss/book/web/LoginBean.java
@@ -1,32 +1,66 @@
 package cz.muni.fi.jboss.book.web;
 
 import java.io.Serializable;
+
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import cz.muni.fi.jboss.book.ejb.security.AccountManager;
+import cz.muni.fi.jboss.book.persistence.entity.User;
+
 /**
- *
+ * 
  * @author matejkobza
  */
 @ManagedBean
 @SessionScoped
 public class LoginBean implements Serializable {
-  
-  private static final long serialVersionUID = 1516686394858785542L;
-  private boolean authenticated = false;
-  
-  public boolean isAuthenticated() {
-    return this.authenticated;
-  }
-  
-  // @TODO implement login
-  public void login() {
-    this.authenticated = true;
-  }
-  
-  //@TODO implement logout
-  public void logout() {
-    this.authenticated = false;
-  }
-  
+
+	private static final long serialVersionUID = 1516686394858785542L;
+
+	@EJB(name = "AccountManager")
+	private AccountManager accountManager;
+
+	private boolean authenticated = false;
+
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isAuthenticated() {
+		return this.authenticated;
+	}
+
+	// @TODO implement login
+	public void login() {
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		
+		if (accountManager.login(user)) {
+			this.authenticated = true;
+		}
+	}
+
+	// @TODO implement logout
+	public void logout() {
+		this.authenticated = false;
+	}
+
 }
