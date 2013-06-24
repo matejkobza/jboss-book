1b673df7dc911741f3f5f6007569eb5c386d7ad0
matejkobza <mates@ravers.sk>
2013-06-24 20:54:11 +0200

    librarian screens

--- jboss-book-web/src/main/java/cz/muni/fi/jboss/book/web/controller/ReservationsBean.java
+++ jboss-book-web/src/main/java/cz/muni/fi/jboss/book/web/controller/ReservationsBean.java
@@ -2,17 +2,19 @@ package cz.muni.fi.jboss.book.web.controller;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.ResourceBundle;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
+import cz.muni.fi.jboss.book.ejb.manager.UserManager;
+import cz.muni.fi.jboss.book.persistence.UserRole;
+import cz.muni.fi.jboss.book.persistence.entity.User;
 import cz.muni.fi.jboss.book.web.core.WebApplication;
 import cz.muni.fi.jboss.book.web.core.WebBeanFactory;
-import org.primefaces.context.RequestContext;
 
 import cz.muni.fi.jboss.book.ejb.manager.ReservationManager;
 import cz.muni.fi.jboss.book.persistence.ReservationState;
@@ -25,40 +27,69 @@ public class ReservationsBean implements Serializable {
     @EJB(name = "ReservationManager")
     private ReservationManager reservationManager;
 
-    private Long reservationState;
-    private Map<String, Long> reservationStates = new HashMap<String, Long>();
-    private List<BookCopyReservation> reservations = null;
+    @EJB(name = "UserManager")
+    private UserManager userManager;
 
-    public ReservationsBean() {
-        // reservationStates.put("all", 0L);
-        reservationStates.put("new", 1L);
-        reservationStates.put("ready", 2L);
-        reservationStates.put("lent", 3L);
-        reservationStates.put("returned", 4L);
+    private String readerName;
+    private User reader;
+
+    public String getReaderName() {
+        return readerName;
     }
 
-    private ReservationState numberToReservationState(Long stateNum) {
-        if (stateNum.equals(1L))
-            return ReservationState.NEW;
-        else if (stateNum.equals(2L))
-            return ReservationState.READY;
-        else if (stateNum.equals(3L))
-            return ReservationState.LENT;
-        else if (stateNum.equals(4L))
-            return ReservationState.RETURNED;
-        else
-            return null;
+    public void setReaderName(String readerName) {
+        this.readerName = readerName;
+    }
+
+    public User getReader() {
+        return reader;
+    }
+
+    public void setReader(User reader) {
+        this.reader = reader;
+    }
+
+    public List<String> complete(String query) {
+        List<String> results = new ArrayList<>();
+
+        for (User user : userManager.searchUsers(query)) {
+            if (user.getUserRole() == UserRole.READER) {
+                results.add(user.getName());
+            }
+        }
+
+        return results;
+    }
+
+    public void searchUser() {
+        if (this.readerName != null) {
+            List<User> users = userManager.searchUsers(this.readerName);
+            if (users.size() == 1) {
+                this.reader = users.get(0);
+            } else {
+                WebApplication.getReference().addErrorMessage("Reservations service", "more than one possibility for user. Please, be more specific.");
+            }
+        }
+    }
+
+    public boolean isUserSelected() {
+        return this.reader != null;
     }
 
     public List<BookCopyReservation> getReservations() {
-        if (reservations == null)
-            reservations = reservationManager.getAllBookCopyReservations(false);
-        return reservations;
+        return reservationManager.getAllBookCopyReservations(false);
     }
 
-    public Long getReservationState() {
-        return reservationState;
+    public List<BookCopyReservation> getActiveReservations() {
+        List<BookCopyReservation> activeReservations = new ArrayList<>();
+        for (BookCopyReservation bookCopyReservation : getReservations()) {
+            if (bookCopyReservation.getReservationState() == ReservationState.NEW) {
+                activeReservations.add(bookCopyReservation);
+            }
+        }
+        return activeReservations;
     }
+
 
     public String getLocalizedReservationState(ReservationState key) {
         if (WebApplication.getReference().getResourceBundle().containsKey(key.toString())) {
@@ -67,63 +98,38 @@ public class ReservationsBean implements Serializable {
         return key.toString();
     }
 
-    public void setReservationState(Long reservationState) {
-        this.reservationState = reservationState;
-    }
-
-    public Map<String, Long> getReservationStates() {
-        return reservationStates;
-    }
-
-    /**
-     * this is not used i think
-     */
-    public void handleReservationStateChange() {
-        ReservationState state = numberToReservationState(this.reservationState);
-        if (state == null) {
-            reservations = null;
-        } else {
-            reservations = reservationManager.getBookCopyReservations(null, state);
-        }
-    }
-
-    private void updateReservation(Long reservationId, ReservationState state) {
-        if (this.reservations == null)
-            return;
-        for (BookCopyReservation reservation : this.reservations) {
-            if (reservation.getId().equals(reservationId)) {
-                reservation.setReservationState(state);
-                break;
-            }
-        }
-    }
-
-    public boolean prepareBook(Long reservationId) {
+    public void prepareBook(Long reservationId) {
         reservationManager.prepareBook(reservationId);
-        // TODO - only if the call succeeded
-        updateReservation(reservationId, ReservationState.READY);
-        RequestContext.getCurrentInstance().update("resultTable");
-        // TODO
-        return true;
     }
 
-    public boolean lendBook(Long reservationId) {
+    public void lendBook(Long reservationId) {
         reservationManager.lendBook(reservationId);
-        // TODO - only if the call succeeded
-        updateReservation(reservationId, ReservationState.LENT);
-        RequestContext.getCurrentInstance().update("resultTable");
-        // TODO
-        return true;
     }
 
-    public boolean returnBook(Long reservationId) {
+    public void returnBook(Long reservationId) {
         reservationManager.returnBook(reservationId);
-        // TODO - only if the call succeeded
-        updateReservation(reservationId, ReservationState.RETURNED);
-        RequestContext.getCurrentInstance().update("resultTable");
-        // TODO
-        return true;
     }
+
+    public void cancelReservation(Long reservationId) {
+        reservationManager.cancelReservation(reservationId);
+    }
+
+
+// Pragma mark - selected reader
+
+    public List<BookCopyReservation> getReaderReservations() {
+        List<BookCopyReservation> reservations = new ArrayList<>();
+        reservations.addAll(reservationManager.getBookCopyReservations(this.reader, ReservationState.READY));
+        reservations.addAll(reservationManager.getBookCopyReservations(this.reader, ReservationState.NEW));
+        return reservations;
+    }
+
+    public List<BookCopyReservation> getReaderBorrowings() {
+        return reservationManager.getBookCopyReservations(this.reader, ReservationState.LENT);
+    }
+
+
+// Pragma mark - current logged in user
 
     public List<BookCopyReservation> getUserReservations() {
         return reservationManager.getBookCopyReservations(WebBeanFactory.getIdentityBean().getUser(), ReservationState.NEW);
--- jboss-book-web/src/main/java/cz/muni/fi/jboss/book/web/controller/SearchBookBean.java
+++ jboss-book-web/src/main/java/cz/muni/fi/jboss/book/web/controller/SearchBookBean.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
 import cz.muni.fi.jboss.book.ejb.entities.BookCopyWithDetails;
 import cz.muni.fi.jboss.book.ejb.manager.BookManager;
 import cz.muni.fi.jboss.book.persistence.entity.Book;
@@ -14,75 +15,78 @@ import cz.muni.fi.jboss.book.persistence.entity.Book;
 @SessionScoped
 public class SearchBookBean implements Serializable {
 
-	@EJB(name = "BookManager")
-	private BookManager bookManager;
+    @EJB(name = "BookManager")
+    private BookManager bookManager;
 
-	private String bookTitle;
-	private Collection<Book> searchBookResults;
-	private Book selectedBook;
-	private List<BookCopyWithDetails> bookCopies;
+    private String bookTitle;
+    private Collection<Book> searchBookResults;
+    private Book selectedBook;
+    private List<BookCopyWithDetails> bookCopies;
 
-	private String authorFirstName;
-	private String authorSurname;
+    private String authorFirstName;
+    private String authorSurname;
 
-	public String getBookTitle() {
-		return bookTitle;
-	}
+    public String getBookTitle() {
+        return bookTitle;
+    }
 
-	public void setBookTitle(String bookTitle) {
-		this.bookTitle = bookTitle;
-	}
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
 
-	public Collection<Book> getSearchBookResults() {
-		return searchBookResults;
-	}
+    public Collection<Book> getSearchBookResults() {
+        return searchBookResults;
+    }
 
-	public void setSearchBookResults(Collection<Book> searchBookResults) {
-		this.searchBookResults = searchBookResults;
-	}
+    public void setSearchBookResults(Collection<Book> searchBookResults) {
+        this.searchBookResults = searchBookResults;
+    }
 
-	public void searchBookByTitlePart() {
-		String title = bookTitle == null ? "" : bookTitle.trim();
-		this.searchBookResults = bookManager.findBookByTitlePart(title);
-	}
-	
-	public void searchBookByAuthor() {
-		String firstName = this.authorFirstName == null ? "" : this.authorFirstName.trim();
-		String surname = this.authorSurname == null ? "" : this.authorSurname.trim();
-		this.searchBookResults = bookManager.findBookByAuthor(firstName, surname);
-	}
+    public void searchBookByTitlePart() {
+        String title = bookTitle == null ? "" : bookTitle.trim();
+        this.searchBookResults = bookManager.findBookByTitlePart(title);
+    }
 
-	public Book getBook() {
-		return this.selectedBook;
-	}
+    public void searchBookByAuthor() {
+        String firstName = this.authorFirstName == null ? "" : this.authorFirstName.trim();
+        String surname = this.authorSurname == null ? "" : this.authorSurname.trim();
+        this.searchBookResults = bookManager.findBookByAuthor(firstName, surname);
+    }
 
-	public void selectBook(Long bookId) {
-		this.selectedBook = bookManager.findBookById(bookId);
-		this.bookCopies = bookManager.getBookCopiesWithDetailsByBookId(bookId);
-	}
+    public Book getBook() {
+        return this.selectedBook;
+    }
 
-	public List<BookCopyWithDetails> getBookCopies() {
-		return this.bookCopies;
-	}
+    public void selectBook(Long bookId) {
+        this.selectedBook = bookManager.findBookById(bookId);
+        //this.bookCopies = bookManager.getBookCopiesWithDetailsByBookId(bookId);
+    }
 
-	public List<Book> getListBooks() {
-		return bookManager.findAllBooks();
-	}
+    public List<BookCopyWithDetails> getBookCopies() {
+        if (this.selectedBook != null) {
+            this.bookCopies = bookManager.getBookCopiesWithDetailsByBookId(this.selectedBook.getId());
+        }
+        return this.bookCopies;
+    }
 
-	public String getAuthorFirstName() {
-		return authorFirstName;
-	}
+    public List<Book> getListBooks() {
+        return bookManager.findAllBooks();
+    }
 
-	public void setAuthorFirstName(String authorFirstName) {
-		this.authorFirstName = authorFirstName;
-	}
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
 
-	public String getAuthorSurname() {
-		return authorSurname;
-	}
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
+    }
 
-	public void setAuthorSurname(String authorSurname) {
-		this.authorSurname = authorSurname;
-	}
+    public String getAuthorSurname() {
+        return authorSurname;
+    }
+
+    public void setAuthorSurname(String authorSurname) {
+        this.authorSurname = authorSurname;
+    }
 
 }
